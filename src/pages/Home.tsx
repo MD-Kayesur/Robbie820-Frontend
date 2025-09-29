034b3fdb81aec5e0cc0dcb59684518099eb00ca3
MD-Kayesur <rmdkayesur@gmail.com>
2025-09-29 23:25:19 +0600

    textascroll animation done

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,41 +1,24 @@
+import ServicesSection from "@/components/test/ServicesSection";
 import CommonWrapper from "../common/CommonWrapper";
-import {
-  decrement,
-  increment,
-  reset,
-} from "@/store/Slices/counterSlice/counterSlice";
-import { useAppDispatch, useAppSelector } from "@/hooks/useRedux";
-
+import ServicesSection2 from "@/components/test/ServicesSection2";
+import ScrollAnimationSection from "@/components/ScrollTextAnimation/ScrollAnimationSection";
+ 
+ 
 const Home = () => {
-  const count = useAppSelector((state) => state.counter.value);
-  const dispatch = useAppDispatch();
+ 
 
   return (
     <CommonWrapper>
       <div className="h-screen bg-website-color-lightGreen">
         <div className="flex flex-col items-center justify-center min-h-screen ">
-          <h1 className="text-2xl font-bold mb-4">Home Page</h1>
-          <h2 className="text-xl font-bold">Counter: {count}</h2>
-          <div className="mt-4 space-x-2">
-            <button
-              className="bg-blue-500 text-white px-4 py-2 rounded"
-              onClick={() => dispatch(increment())}
-            >
-              Increment
-            </button>
-            <button
-              className="bg-red-500 text-white px-4 py-2 rounded"
-              onClick={() => dispatch(decrement())}
-            >
-              Decrement
-            </button>
-            <button
-              className="bg-gray-500 text-white px-4 py-2 rounded"
-              onClick={() => dispatch(reset())}
-            >
-              Reset
-            </button>
-          </div>
+          <h1 className="text-2xl font-bold mb-4"> this is only for practice  
+
+            
+          </h1>
+          
+          {/* <ServicesSection/> */}
+          {/* <ServicesSection2/> */}
+          <ScrollAnimationSection></ScrollAnimationSection>
         </div>
       </div>
     </CommonWrapper>
