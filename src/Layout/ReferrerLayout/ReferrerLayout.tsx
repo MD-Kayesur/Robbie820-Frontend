150c6f98d470d0f8894d614ce5180eb5400cc77a
Shahid Hasan Shuvo <mrshanshuvo@gmail.com>
2026-03-05 12:32:54 +0600

    Completed UI part

--- src/Layout/ReferrerLayout/ReferrerLayout.tsx
+++ src/Layout/ReferrerLayout/ReferrerLayout.tsx
@@ -4,7 +4,7 @@ import ReferrerTopbar from "./ReferrerTopbar";
 
 const ReferrerLayout = () => {
   return (
-    <div className="h-screen w-full bg-[#fdfdfd] flex overflow-hidden font-sans selection:bg-sky-100 selection:text-sky-900">
+    <div className="h-screen w-full bg-[#fdfdfd] flex overflow-hidden selection:bg-sky-100 selection:text-sky-900">
       <ReferrerSidebar />
       <div className="flex-1 flex flex-col h-full overflow-hidden">
         <ReferrerTopbar />
