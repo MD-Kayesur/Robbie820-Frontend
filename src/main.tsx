a948e86032b41c142a455e85b23b4f620d36396c
MD-Kayesur <rmdkayesur@gmail.com>
2026-04-13 08:58:12 +0600

    update routes import path to match renamed Routes file

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
-import routes from "./routes/routes";
+import routes from "./routes/Routes";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
